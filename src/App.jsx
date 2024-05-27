f317c9ed01c9102c2a3c6e23335771b33585cdb9
NadaElho <nadaelhosary51@gmail.com>
2024-05-27 17:41:03 +0300

    booking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-// import LanguageSwitch from "./components/LanguageSwitch";
+import LanguageSwitch from "./components/LanguageSwitch";
 import ModeSwitch from "./components/ModeSwitch";
 import LanguageProvider from "./providers/LanguageContext";
 import { I18nextProvider } from "react-i18next";
 import { useState } from "react";
 import i18n from "./utils/i18n.js";
+import BookingForm from "./pages/BookingForm.jsx";
+import { Route, Routes } from "react-router-dom";
 function App() {
   const [dark, setDark] = useState(localStorage.getItem("dark") || "light");
-  const handleMode  =()=>{
-    setDark((mode)=> mode === 'light' ? 'dark' : 'light')
-  }
+  const handleMode = () => {
+    setDark((mode) => (mode === "light" ? "dark" : "light"));
+  };
   return (
     <>
-      <div className= {`${dark} dark:bg-black dark:text-white` }>
-        <h1>hello</h1>
+      <div className={`${dark} dark:bg-black bg-grey-100 dark:text-white p-4 min-h-screen`}>
         <I18nextProvider i18n={i18n}>
           <LanguageProvider>
-            <ModeSwitch mode={handleMode}/>
-            {/* <LanguageSwitch /> */}
+            <ModeSwitch mode={handleMode} />
+            <LanguageSwitch />
+            <Routes>
+              <Route path="reservation-room/:id" element={<BookingForm />} />
+            </Routes>
           </LanguageProvider>
         </I18nextProvider>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode:"selector",
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: 'selector',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main:{
+          100: '#AA9383',
+          200: '#997c6a',
+          300: '#997c6a',
+          400: '#81664b',
+          800: '#52381D'
+        },
+        grey:{
+          100: '#fff7f2',
+          500: '#C5BDBA',
+        }
+      },
+    },
   },
   plugins: [],
 }
