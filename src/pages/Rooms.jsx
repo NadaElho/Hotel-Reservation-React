1e0595dbdb26e616bb5f1dc25aa8c17d4823be79
esraaali138 <esraaali1999138@gmail.com>
2024-06-02 14:29:13 -0700

    add

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -14,18 +14,14 @@ const Rooms = () => {
 
   useEffect(() => {
     async function fetchData() {
-      try {
         const res = await axios.get("http://localhost:3000/api/v1/rooms", {
           params: {
             "price[gt]": value[0],
-            "price[lt]": value[1]
-          }
+            "price[lt]": value[1],
+          },
         });
         const data = res.data.data;
         setRooms(data);
-      } catch (error) {
-        console.error("Error fetching rooms:", error);
-      }
     }
     fetchData();
   }, [value]);
@@ -44,8 +40,7 @@ const Rooms = () => {
                   type="text"
                   max="10000"
                   value={value[0]}
-                  onChange={(event)=>setValue([event.target.value === "" ? 0 : parseInt(event.target.value),value[1]])}
-                  //pareseint === min but value[1] = max 
+                  onChange={(event) => setValue([event.target.value === "" ? 0 : parseInt(event.target.value), value[1]])}
                   className="w-full border-none border-b-2 border-black focus:outline-none focus:border-custom-500"
                 />
               </div>
@@ -54,7 +49,7 @@ const Rooms = () => {
                 <input
                   type="text"
                   value={value[1]}
-                  onChange={(event)=> setValue([ value[0],event.target.value === "" ? 40 : parseInt(event.target.value)])}
+                  onChange={(event) => setValue([value[0], event.target.value === "" ? 10000 : parseInt(event.target.value)])}
                   min="0"
                   className="w-full border-none border-b-2 border-black focus:outline-none focus:border-blue-500"
                 />
@@ -71,51 +66,50 @@ const Rooms = () => {
           </div>
         </div>
         <div className="flex flex-wrap gap-6">
-          {rooms.map((room) => (
-            <div
-              className="w-full sm:max-w-96 rounded-3xl overflow-hidden shadow-lg border border-secondary border-opacity-40 "
-              key={room._id}
-            >
-              <img
-                className="w-full h-64 object-cover"
-                src="https://images.unsplash.com/photo-1611892440504-42a792e24d32?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww"
-                alt=""
-              />
-              <div className="px-6 py-4">
-                <div className="font-bold text-2xl mb-2 text-primary">
-                  {room.roomTypeId.type_en}
+          {rooms.length > 0 ? (
+            rooms.map((room) => (
+              <div
+                className="w-full sm:max-w-96 rounded-3xl overflow-hidden shadow-lg border border-secondary border-opacity-40"
+                key={room._id}
+              >
+                <img
+                  className="w-full h-64 object-cover"
+                  src="https://images.unsplash.com/photo-1611892440504-42a792e24d32?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww"
+                  alt=""
+                />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-2xl mb-2 text-primary">
+                    {room.roomTypeId.type_en}
+                  </div>
+                  <p className="text-primary opacity-80 font-semibold text-sm text-justify tracking-tight mt-4">
+                    {room.description_en}
+                  </p>
+                  <hr className="bg-primary mt-4" />
                 </div>
-                <p className="text-primary opacity-80 font-semibold text-sm text-justify tracking-tight mt-4">
-                  {room.description_en}
-                </p>
-                <hr className="bg-primary mt-4" />
-              </div>
-              <div className="px-6 pt-4 pb-2 text-center">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                </span>
-                <hr className="bg-primary" />
-                <div className="w-full flex justify-between py-8">
-                  <button className="w-40 bg-primary text-white text-sm opacity-95 py-3 px-4 rounded-full inline-flex items-center">
-                    <Link to="/reservation-room/:id">
-                      Book now for ${room.price}
-                    </Link>
-                  </button>
-                  <button className="w-40 bg-transparent border border-primary rounded-full text-primary opacity-95 font-semibold py-2 px-4 inline-flex items-center justify-center">
-                    <Link to={`/rooms/${room._id}`}>Check details</Link>
-                  </button>
+                <div className="px-6 pt-4 pb-2 text-center">
+                  <hr className="bg-primary" />
+                  <div className="w-full flex justify-between py-8">
+                    <button className="w-40 bg-primary text-white text-sm opacity-95 py-3 px-4 rounded-full inline-flex items-center">
+                      <Link to={`/reservation-room/${room._id}`}>
+                        Book now for ${room.price}
+                      </Link>
+                    </button>
+                    <button className="w-40 bg-transparent border border-primary rounded-full text-primary opacity-95 font-semibold py-2 px-4 inline-flex items-center justify-center">
+                      <Link to={`/rooms/${room._id}`}>Check details</Link>
+                    </button>
+                  </div>
                 </div>
               </div>
+            ))
+          ) : (
+            <div className="text-center w-full mt-10">
+              <p className="text-2xl text-primary text center font-semibold">No rooms found matching your criteria.</p>
             </div>
-          ))}
+          )}
         </div>
       </div>
-      {/* room */}
     </>
   );
 };
 
 export default Rooms;
-
-
-
-
