68a9eed0dbf1754f9eeea4e2f9f99eac266779bb
ayael <tweetyelsayed656@gmail.com>
2024-06-05 02:53:19 +0300

    branch

--- src/pages/Branch.jsx
+++ src/pages/Branch.jsx
@@ -1,155 +1,156 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import Branches from "./Branches";
 export default function Branch() {
-  var settings = {
+  const [branch, setBranch] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await axios.get(
+          `http://localhost:3000/api/v1/hotels/${id}`
+        );
+        const data = res.data.data;
+        setBranch(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, [id]);
+
+  const sliderSettings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
-  var setting = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-  };
+
   return (
     <>
-      <div className="flex flex-col md:flex-row">
-        <div className="container flex-1 md:w-64 w-full">
-          <img
-            className="w-full md:w-auto"
-            src="./public/assets/aboutbranch.png"
-            alt="About Branch"
-          />
-        </div>
-        <div className="container flex-1 md:w-32 w-full flex flex-col items-center justify-center">
-          <h1 className="text-center mt-10 text-5xl md:text-5xl playfair-display text-main-800 font-semibold">
-            About Cairo Branch
-          </h1>
-          <p className="text-start m-5 md:m-10 text-lg md:text-xl playfair-display text-main-800 font-medium">
-            A charming retreat nestled in the heart of Zamalek, Cairo's vibrant
-            island neighbourhood. Our hotel offers a fusion of modern comforts
-            and traditional elegance, providing guests with a memorable stay in
-            this bustling metropolis.
-          </p>
-          <p className="text-start m-5 md:m-10 text-lg md:text-xl playfair-display text-main-800 font-medium">
-            Step into our tranquil oasis and experience impeccable hospitality
-            from the moment you arrive. Whether you're visiting for business or
-            leisure, our dedicated staff is committed to ensuring your every
-            need is met, from personalized service to thoughtful amenities.
-          </p>
-          <a className="btn btn-wide rounded-3xl bg-main-800 text-white text-center mt-5 hover:bg-main-400 hover:text-white">
-            Book now
-          </a>
-        </div>
-      </div>
+      {branch ? (
+        <>
+          <div className="flex flex-col md:flex-row my-5 container">
+            <div className="flex-1 w-full md:w-64">
+              <img
+                className="w-full md:w-auto"
+                src={branch.images[0]}
+                alt="About Branch"
+              />
+            </div>
+            <div className="flex-1 w-full md:w-32 flex flex-col items-center justify-center">
+              <h1 className="text-center mt-10 text-5xl playfair-display text-main-800 font-semibold">
+                About {branch.name_en} Branch
+              </h1>
+              <p className="text-start m-5 md:m-10 text-lg md:text-xl playfair-display text-main-800 font-medium">
+                {branch.description_en}
+              </p>
+              <button
+                className="w-72 rounded-3xl bg-main-800 text-white px-4 py-2 text-center mt-5 hover:bg-main-400 hover:text-white"
+                onClick={() => navigate("/bookingform")}
+              >
+                Book now
+              </button>
+            </div>
+          </div>
 
-      <div className="flex flex-col md:flex-row">
-        <div className="container flex-1 w-full md:w-64 order-1 md:order-2">
-          <img
-            className="w-auto"
-            src="./public/assets/view-accomodations.png"
-            alt="About Branch"
-          />
-        </div>
-        <div className="container flex-1 w-full md:w-32 flex flex-col items-center justify-center order-2 md:order-1">
-          <h1 className="text-center mt-10 text-5xl playfair-display text-main-800 font-semibold">
-            Sleep with us
-          </h1>
-          <p className="text-start m-10 text-xl playfair-display text-main-800 font-medium">
-            Welcome to our Rooms at [Hotel Name] in Zamalek, Cairo. Each of our
-            thoughtfully designed accommodations offers a perfect blend of
-            comfort, style, and convenience to ensure a memorable stay for every
-            guest.
-          </p>
-          <p className="text-start m-10 text-xl playfair-display text-main-800 font-medium">
-            Welcome to our Rooms at [Hotel Name] in Zamalek, Cairo. Each of our
-            thoughtfully designed accommodations offers a perfect blend of
-            comfort, style, and convenience to ensure a memorable stay for every
-            guest.
-          </p>
-          <a className="btn btn-wide rounded-3xl bg-main-800 text-white text-center mt-5 hover:bg-main-400 hover:text-white">
-            View Accommodations
-          </a>
-        </div>
-      </div>
+          <div className="flex flex-col md:flex-row">
+            <div className="flex-1 w-full md:w-64 order-1 md:order-2 my-5">
+              <img
+                className="w-auto rounded-3xl"
+                src={branch.images[1]}
+                alt="View Accommodations"
+              />
+            </div>
+            <div className="flex-1 w-full md:w-32 flex flex-col items-center justify-center order-2 md:order-1">
+              <h1 className="text-center mt-10 text-5xl playfair-display text-main-800 font-semibold">
+                Sleep with us
+              </h1>
+              <p className="text-start m-10 text-xl playfair-display text-main-800 font-medium">
+                The rooms in our hotel are meticulously designed to offer a
+                sanctuary of comfort and relaxation. Each room boasts plush
+                bedding with high-thread-count linens, ensuring a restful
+                night’s sleep. The ambient lighting, coupled with carefully
+                selected décor, creates a soothing atmosphere perfect for
+                unwinding after a long day.
+                <br />
+                <address>{branch.address_en}</address>
+                {branch.phoneNumber[0]}
+              </p>
 
-      <div className="mt-10">
-        <Slider {...settings}>
-          <div>
-            <img src="./public/assets/slider1.png"></img>
-          </div>
-          <div>
-            <img src="./public/assets/slider2.png"></img>
-          </div>
-          <div>
-            <img src="./public/assets/slider3.png"></img>
-          </div>
-          <div>
-            <img src="./public/assets/slider4.png"></img>
-          </div>
-          <div>
-            <img src="./public/assets/slider5.png"></img>
-          </div>
-          <div>
-            <img src="./public/assets/slider6.png"></img>
-          </div>
-        </Slider>
-      </div>
-      <div>
-        <h1 className="text-center mt-52 text-5xl playfair-display text-main-800 font-semibold">
-          Check out our other branches
-        </h1>
-        <div className="container mx-auto  ">
-          <Slider {...setting}>
-            <div className="w-96 h-full mx-10 mt-10 ">
-              <img
-                className="h-full w-80 object-cover"
-                src="/assets/img1.png"
-              />
+              <button
+                className="w-72 rounded-3xl px-4 py-2 bg-main-800 text-white text-center mt-5 hover:bg-main-400 hover:text-white"
+                onClick={() => navigate("/rooms")}
+              >
+                View Accommodations
+              </button>
             </div>
-            {/*  */}
-            <div className="w-96 h-full mx-10 mt-40">
-              <img
-                className="h-full w-80 object-cover"
-                src="/assets/img2.png"
-              />
-            </div>
-            {/*  */}
-            <div className="w-96 h-full mx-10 mt-10">
-              <img
-                className="h-full w-80 object-cover"
-                src="/assets/img3.png"
-              />
-            </div>
-            {/*  */}
-            <div className="w-96 h-full mx-10 mt-40">
-              <img
-                className="h-full w-80 object-cover"
-                src="/assets/img4.png"
-              />
-            </div>
-            {/*  */}
-            <div className="w-96 h-full mx-10 mt-10">
-              <img
-                className="h-full w-80 object-cover"
-                src="/assets/img5.png"
-              />
-            </div>
-            {/*  */}
-            <div className="w-96 h-full mx-10 mt-40">
-              <img
-                className="h-full w-80 object-cover"
-                src="/assets/img6.png"
-              />
-            </div>
-          </Slider>
-        </div>
-      </div>
+          </div>
+
+          <div className="mt-10 mx-8">
+            <Slider {...sliderSettings}>
+              <div>
+                <img
+                  className="w-auto"
+                  src="/assets/slider1.png"
+                  alt="Slider 1"
+                />
+              </div>
+              <div>
+                <img
+                  className="w-auto"
+                  src="/assets/slider2.png"
+                  alt="Slider 2"
+                />
+              </div>
+              <div>
+                <img
+                  className="w-auto"
+                  src="/assets/slider3.png"
+                  alt="Slider 3"
+                />
+              </div>
+              <div>
+                <img
+                  className="w-auto"
+                  src="/assets/slider4.png"
+                  alt="Slider 4"
+                />
+              </div>
+              <div>
+                <img
+                  className="w-auto"
+                  src="/assets/slider5.png"
+                  alt="Slider 5"
+                />
+              </div>
+              <div>
+                <img
+                  className="w-auto"
+                  src="/assets/slider6.png"
+                  alt="Slider 6"
+                />
+              </div>
+            </Slider>
+          </div>
+
+          <div>
+            <h1 className="text-center mt-52 text-5xl playfair-display text-main-800 font-semibold">
+              Check out our other branches
+            </h1>
+            <Branches />
+          </div>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </>
   );
 }
--- src/pages/Branches.jsx
+++ src/pages/Branches.jsx
@@ -18,41 +18,58 @@ const Branches = () => {
   }, []);
 
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
-    className: "center",
-    centerPadding: "100px",
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <>
-      <div className=" mx-20  flex justify-between mt-36 ">
-        <h2 className="text-primary text-4xl font-secondary uppercase font-bold">
+      <div className="mx-4 md:mx-10 lg:mx-20 flex flex-col lg:flex-row justify-between mt-16 md:mt-24 lg:mt-36 space-y-6 lg:space-y-0">
+        <h2 className="text-primary text-2xl md:text-3xl lg:text-4xl font-secondary uppercase font-bold">
           Discover our Branches
         </h2>
-        <p className="w-64 py-1 text-primary font-custom font-semibold">
+        <p className="w-full lg:w-64 py-1 text-primary font-custom font-semibold">
           Embark on an unforgettable journey. Experience our unique culture, and
           create lifetime memories. Your adventure starts here!
         </p>
       </div>
+
       <div className="container mx-auto mt-8">
         <Slider {...settings}>
           {branches.map((branch) => (
             <Link
               key={branch.id}
               to={`branch/${branch._id}`}
-              className="relative rounded-t-full rounded-3xl cursor-pointer overflow-hidden mx-2 h-[480px] md:max-w-80 sm:max-w-full sm:mx-10  "
+              className="relative rounded-t-full rounded-3xl cursor-pointer overflow-hidden mx-2 h-[480px] md:max-w-80 sm:max-w-full sm:mx-10"
             >
               <img
-                src={branch.images[1]}
-                alt=""
-                className="h-full w-full object-cover"
+                src={branch.images[2]}
+                alt={branch.name_en}
+                className="h-full w-auto object-cover"
               />
               <div className="absolute bottom-6 left-6 w-full text-white">
-                <p className="font-400 text-3xl font-secondary">{branch.name_en}</p>
+                <p className="font-400 text-3xl font-secondary">
+                  {branch.name_en}
+                </p>
                 <p className="opacity-80">{branch.address_en}</p>
               </div>
             </Link>
