cc382e8f279d06c5391410545d9a345a76bc7a4a
NadaElho <nadaelhosary51@gmail.com>
2024-06-23 19:17:59 +0300

    discount

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -4,25 +4,21 @@ import Card from "../components/Card";
 import Loader from "../components/Loader";
 
 const Favourites = () => {
-  const [favouriteRooms, setFavouiteRooms] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const userId = localStorage.getItem("userId");
+
   useEffect(() => {
     (async function () {
-      const { data } = await axiosInstance.get(
-        `/rooms/favourites/${localStorage.getItem("userId")}`
-      );
-      let roomsId = data.data.map((reservationUser) => reservationUser._id);
-      const roomsResponse = await axiosInstance.get(
-        `/rooms?roomsId=${roomsId.join(",")}`
-      );
-      setFavouiteRooms(roomsResponse.data.data);
+      const { data } = await axiosInstance.get(`/users/${userId}`);
+      setUserData(data.data);
     })();
-  }, []);
+  }, [userId]);
 
   return (
     <div className="flex justify-center flex-wrap gap-4">
-      {favouriteRooms ? (
-        favouriteRooms.map((room) => {
-          return <Card room={room} key={room._id} />;
+      {userData && userData.favouriteRooms ? (
+        userData.favouriteRooms.map((room) => {
+          return <Card room={room} key={room._id} userData={userData}/>;
         })
       ) : (
         <div className="h-screen">
